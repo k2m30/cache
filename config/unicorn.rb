9ba9a81c7501d30b2e51edff8176b274f6bde0f5
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2014-02-05 15:44:49 +0300

    deploy

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,9 @@
-root = '/home/lagutko/apps/distributor/current'
+root = '/home/lagutko/apps/cache/current'
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen '/tmp/unicorn.distributor.sock'
+listen '/tmp/unicorn.cache.sock'
 worker_processes 2
 timeout 30
